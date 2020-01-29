cf37fa18a732d272d867dfdbb545d60d351e02cb
Pascal de Bruijn <p.debruijn1@student.avans.nl>
2020-01-29 13:04:23 +0100

    first test added

---
+++ test1/test1/main.c
@@ -0,0 +1,18 @@
+/*
+ * test1.c
+ *
+ * Created: 29-1-2020 12:37:16
+ * Author : HUIBTECH
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
